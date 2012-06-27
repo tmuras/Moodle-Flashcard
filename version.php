758d07e7820355ae4cfe5b0e340fac97a9e1c3cc
Tomasz Muras <nexor1984@gmail.com>
2012-06-27 21:35:08 +0200

    Version bump, required Moodle is 2.2 now.

--- version.php
+++ version.php
@@ -7,8 +7,8 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$module->version  = 2012040100;  // The current module version (Date: YYYYMMDDXX)
-$module->requires = 2011000000;  // Requires this Moodle version
+$module->version  = 2012062700;  // The current module version (Date: YYYYMMDDXX)
+$module->requires = 2011120500;  // Requires this Moodle version
 $module->cron     = 3600; // Period for cron to check this module (secs)
 $module->component = 'mod_flashcard'; // Full name of the plugin (used for diagnostics)
 
