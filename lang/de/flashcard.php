fed21bbae0fdfa0479fe0e864f93dd84bc31d187
Alexander Kiy <alekiy@uni-potsdam.de>
2013-10-04 15:10:18 +0200

    added german translation

---
+++ lang/de/flashcard.php
@@ -0,0 +1,182 @@
+<?php
+/**
+ * Strings for component 'flashcard', language 'de'
+ *
+ * @package    mod
+ * @subpackage flashcard
+ * @copyright  Valery Fremaux, Tomasz Muras
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * Deutsche Übersetzung:
+ * Marco Bahrs und Jörg Kapelle
+ * Im Auftrag von:
+ * Universität Potsdam
+ * Am Neuen Palais 10
+ * 14469 Potsdam 
+ */
+
+$string['addone'] = 'Frage hinzufügen';
+$string['addthree'] = 'drei neue Fragen hinzufügen';
+$string['answer'] = 'Antworten';
+$string['answersmediatype'] = 'Fragetyp';
+$string['autodowngrade'] = 'Automatisches Herabstufen';
+$string['avgaccess'] = 'Mean ticks per card ';
+$string['backside'] = 'Rückseite Lernkarte';
+$string['backtocourse'] = 'Zurück zum Kurs';
+$string['backtodecks'] = 'Zurück zu den Kartenstapeln';
+$string['badcards'] = 'Karte(n) abstoßen: ';
+$string['bycards'] = 'Nach Karten';
+$string['byusers'] = 'Nach Teilnehmern';
+$string['card'] = 'Karten';
+$string['cardsindeck'] = 'Verbleibend : {$a} Karte(n)';
+$string['cardsep'] = 'Card Separator in text import:';
+$string['cardsremaining'] = 'Verbleibende Karte(n)';
+$string['cardsread'] = 'Card(s) read: ';
+$string['commands'] = '';
+$string['confirm'] = 'Bestätigen:';
+$string['counters'] = 'Zähler';
+$string['customisation'] = 'To customize this instance of flashcard, get the stylesheet beneath, <br/>edit it and save it within the <a href=\"{$a}\">moddata/flashcard</a>/&lt;<i>instance</i>&gt; directory in the course\'s files.<br/> If you use images as background, save them in the same place, and invoke them with their simple names';
+$string['deck'] = 'Kartenstapel';
+$string['deck1_delay'] = 'Markierung zur Wiederholung (Stapel 1)';
+$string['deck2_delay'] = 'Markierung zur Wiederholung (Stapel 2)';
+$string['deck2_release'] = 'Zeit bis Herabstufung (Stapel 2)';
+$string['deck3_delay'] = 'Markierung zur Wiederholung (Stapel 3)';
+$string['deck3_release'] = 'Zeit bis Herabstufung (Stapel 3)';
+$string['deck4_delay'] = 'Markierung zur Wiederholung (Stapel 4)';
+$string['deck4_release'] = 'Zeit bis Herabstufung (Stapel 4)';
+$string['deck_delay'] = 'Markierung zur Wiederholung';
+$string['deck_release'] = 'Zeit bis Herabstufung';
+$string['decks'] = 'Lernkartenstapel';
+$string['deckstates'] = 'Decks state';
+$string['deleteselection'] = 'lösche Auswahl';
+$string['difficultcards'] = 'Schwer';
+$string['easycards'] = 'Leicht';
+$string['edit'] = 'Karten bearbeiten';
+$string['editingcards1'] = 'Lernkartenstapel bearbeiten';
+$string['editingcards2'] = 'Lernkartenstapel bearbeiten';
+$string['emptyset'] = 'Lernkartenstapel ist leer';
+$string['endtime'] = 'Zeitgrenze';
+$string['fieldsep'] = 'Field separator:';
+$string['flashback:manage'] = 'Manage the cards';
+$string['flashcard:canplayfree'] = 'Jouer en mode libre';
+$string['flashcard:manage'] = 'Gérer les jeux';
+$string['flipdeck'] = 'Fragen und Antworten vertauschen';
+$string['forcereload'] = 'erzwinge erneutes Laden von Fragen';
+$string['freegame'] = 'Freies Spiel';
+$string['freeplayinstructions'] = 'Betrachten Sie jede Karte im Stapel. Wenn Sie denken, Sie wissen, was der Begriff bedeutet, klicken Sie darauf, um die Karte umzudrehen und überprüfen Sie Ihre Antwort. Wenn diese richtig ist, klicken Sie auf "Ich wusste es!", um sie von dem Stapel zu entfernen. Wenn Sie nicht wissen, was die Antwort ist oder Sie diese falsch beantwortet haben, klicken Sie auf "Ich hab\'s vergessen!", um zur nächsten Karte zu gelangen. Wenn Sie durch alle Karten gegangen sind, überprüfen Sie die Kursmaterialien für Informationen über Begriffe, die Sie nicht kennen und versuchen es erneut.';
+$string['frontside'] = 'Kartenvorderseite';
+$string['goodcards'] = 'gültige Karte(n) : ';
+$string['havetologin'] = 'Sie müssen sich anmelden, bevor Sie Ihre Antworten einreichen können.';
+$string['ifailed'] = 'Ich hab\'s vergessen!';
+$string['igotit'] = 'Ich wusste es!';
+$string['image'] = 'Bild';
+$string['imageplussound'] = 'Bild + Ton';
+$string['import'] = 'Importieren';
+$string['importingcards'] = 'Karten importieren';
+$string['importadvice'] = 'Beim Import von einem neuen Kartenstapel, werden alle alten Kartenstapel, zusammen mit den vorherigen Informationen über die Spiele der Teilnehmer, gelöscht';
+$string['imported'] = 'Import:';
+$string['importreport'] = '<b>Flashcard a importé des nouvelles cartes :</b>';
+$string['instructions'] = 'Klicken Sie auf die Lernkarte, um sie umzudrehen und die andere Seite zu sehen.';
+$string['leitnergame'] = 'Leitner-Spiel';
+$string['matchingquestion'] = 'Passende Frage von der Fragesammlung';
+$string['maxaccess'] = 'More viewed ticks ';
+$string['mediatypes'] = 'Medientyp';
+$string['mediumeffortcards'] = 'Mittel';
+$string['minaccess'] = 'Less viewed ticks ';
+$string['missingimage'] = '{Bild-Datei fehlt}';
+$string['missingsound'] = '{Ton-Datei fehlt}';
+$string['modulename'] = 'Lernkartei';
+$string['modulename_help'] = '<p>
+Die Lernkartei ist ein Hilfsmittel zum systematischen Lernen. Hierzu wird auf die Vorderseite einer 
+Karte ein Stichwort (z. B. Vokabel, Datum, etc.) geschrieben und auf die Rückseite die Lösung 
+(der Fakt, der gelernt werden soll). Mit Hilfe mehrerer Stapel ist es möglich, genau die Fakten zu 
+wiederholen, die noch nicht verinnerlicht wurden.
+</p>
+<p>
+Dieses Modul umfasst eines von Sebastian Leitner entwickeltes System zum systematischen Lernen von Beziehungen.
+Die Lernpaare werden anhand eines Kartenstapels implementiert. Der Benutzer kann zwischen 2, 3 oder 4 Stapel auswählen.
+Wenn der Teilnehmer an der Beantwortung einer Frage scheitert, bleibt diese Karte im selben Stapel. Wenn die Frage 
+richtig beantwortet wurde, wird die Karte in den nächsten Stapel bewegt, so dass das schwierig zu Erlernende öfters wiederholt wird.
+Dieser Prozess wird für alle Stapel durchlaufen bis der Teilnehmer den letzten Stapel absolviert hat. Die Stapel 
+können mit einer Wiederholungszeit versehen werden, damit schwierigere Fragen öfters wiederholt werden können. Die Zeit
+kann individuell eingestellt werden.  
+
+</p>
+<p> 
+Wenn der Kursleiter die Wiederholung eingeschaltet hat, so werden die Karten bei nicht Ausführen der Lernkarten in den nächst niedrigeren Stapel eingestuft.
+</p>
+<p>
+Die Aktivität ermöglicht auch frei zu "spielen" und die Lernkarten "einfach so" durchzugehen. 
+</p>';
+$string['modulenameplural'] = 'Lernkarten';
+$string['next'] = 'Nächste Karte';
+$string['nocards'] = 'Bisher wurden keine Karten erstellt.';
+$string['noflashcards'] = 'Keine Lernkarten in diesem Kurs.';
+$string['nomorecards'] = 'Glückwunsch! Sie haben alle Karten aus diesem Stapel betrachtet.';
+$string['nosubquestions'] = 'Fehler: Fehlende Teilfragen für diese Frage!';
+$string['notinitialized'] = 'Der Teilnehmer hat niemals zuvor diese Lernkarte genutzt.';
+$string['nousers'] = 'Keine Teilnehmer in diesem Kurs.';
+$string['num'] = 'N°';
+$string['numericrequired'] = 'Eine Zahl wird benötigt.';
+$string['outoftimerange'] = 'Dieses Modul wird von einer Zeitspanne begrenzt. Sie können es gerade nicht benutzen.';
+$string['playwithme'] = 'lerne mit diesem Stapel';
+$string['pluginadministration'] = 'Lernkartei Administration.';
+$string['pluginname'] = 'Lernkartei';
+$string['previous'] = 'Vorherige Karte';
+$string['question'] = 'Fragen';
+$string['questionid'] = 'Fragen importieren';
+$string['questionsmediatype'] = 'Fragetyp';
+$string['reinforce'] = 'Stapel überprüfen';
+$string['removecard'] = 'Lösche Karte';
+$string['reset'] = 'Stapel zurücksetzen';
+$string['sound'] = 'Ton';
+$string['sourcequestion'] = 'importieren neuer Fragen';
+$string['starttime'] = 'Startzeit ';
+$string['stylesheet'] = 'Obtain the stylesheet';
+$string['styling'] = 'Benutzerdefiniert';
+$string['sumaccess'] = 'insgesamt betrachtete Karten ';
+$string['summary'] = 'Lernkartei Zusammenfassung';
+$string['teachersummary'] = 'Zusammenfassung';
+$string['text'] = 'Text';
+$string['timetoreview'] = '{$a} verbleibenden Tage bis zur Wiederholung';
+$string['trivialcards'] = 'Sehr leicht';
+$string['undefinedquestionset'] = 'Es wurde keine passende Frage für diese Lernkarte gewählt.';
+$string['userdecks'] = 'Teilnehmer\' Stapel';
+$string['viewed'] = 'Betrachter';
+
+/* Strings for the help section */
+$string['sourcequestion_help'] = 'Choose a question as a source.
+
+You may quickly setup a flashcard deck from a matching question of the question base. To proceed, choose a question in the list and check the "force reload" checkbox. All previous cards will be deleted and the flashcard will be reloaded with the question data.
+
+Warning : changing cards using card editor WILL NOT change back question content in the question base.';
+$string['forcereload_help'] = 'Force Reloading Cardset
+
+Checking this checkbox will force the cardset to be reloaded with data from the choosen matching question.
+
+All previously recorded user data will be deleted and all accounts will be reset.';
+$string['mediatypes_help'] = 'This list defines the type of media used for questions.
+
+* Text: The text entered is literally printed on the cards
+* Image: The text entered is the location of an image file on the server, taken from the course files.
+* Sound: The text entered is the location of a sound file on the server, taken from the course files.';
+$string['flipdeck_help'] = 'Diese Einstellung ermöglicht das Umdrehen der Karten. Die Fragen werden zu Antworten und umgekehrt.';
+$string['decks_help'] = 'Ändert die Anzahl der Stapel (2, 3, oder 4).
+
+Meißt nutzt das Leitner-Lern-System 3 Stapel. Bei 4 Stapeln wird der Lernprozess verlängert und es können größere Lernkarteien gelernt werden. 2 Stapel sind hingegen gut geeignet für kleinere Mengen von Lernkarten.';
+$string['autodowngrade_help'] = 'Diese Option aktiviert das automatische Herabstufen der Karten. Ein Stapel wird zurückgestuft, wenn der Lerner ihn in der angegebenen Zeit nicht wieder betrachtet hat. Längere Zeit nicht betrachtete Karten werden also wieder als schwerer eingestuft und müssen wieder gelernt werden. Die Zeit kann für jeden Stapel einzeln eingestellt werden.';
+$string['deck_release_help'] = 'Vergangene Zeit in Stunden, nach der die Karten in den nächst niedrigeren Stapel herabgestuft werden. Die Zeit für jeden Stapel kann dabei seperat festgelegt werden.';
+$string['deck_delay_help'] = 'Einstellbare Zeit in Stunden, nach denen der Lerkartenstapel wieder als "noch nicht gelernt" markiert wird.';
+$string['import_help'] = "You may massively import question/Answer pairs within a flashcard using the
+card bulk import feature.
+
+You just need to make a file with a list of questions/answers pairs and paste the content of that file in the importer's textarea.
+You may decide to encode this file with some kinds of common separators, on Linux or
+Windows systems. The most common may be \"one pair by line\" files, using ':' or comma to
+separate questions from answers.
+
+Questions will only comme first, and MUST always have an answer. Empty lines or black lines will be ignored, also those starting with a # or a slash (/).
+
+The importer will make a fast check of the text you want to import. It will reject
+the import if the text sequence is not well formed and tell you how many pairs are wrong in the file.
+
+As successfuly importing will erase all data from the decks but also belonging to participants, a confirmation checkbox needs to be checked in to force the import to be performed.";
