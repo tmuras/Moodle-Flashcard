d567bdaccef5622cfbb33bda813c77c4e3f186de
Alexander Kiy <alekiy@uni-potsdam.de>
2013-10-08 10:54:28 +0200

    removed warning with adding StdClass for cards

--- playview.controller.php
+++ playview.controller.php
@@ -36,6 +36,7 @@ if ($action == 'reset'){
 }
 /*---------------------------------- a card was declared right --------------------*/
 if ($action == 'igotit'){       
+	$card = new StdClass();
     $card->id = required_param('cardid', PARAM_INT);
     $card = $DB->get_record('flashcard_card', array('id' => $card->id));    
     if ($card->deck < $flashcard->decks){
@@ -56,6 +57,7 @@ if ($action == 'igotit'){
 }
 /*------------------------------ a card was declared wrong -----------------------*/
 if ($action == 'ifailed'){
+	$card = new StdClass();
     $card->id = required_param('cardid', PARAM_INT);
     $card = $DB->get_record('flashcard_card', array('id' => $card->id));
     $card->lastaccessed = time();
